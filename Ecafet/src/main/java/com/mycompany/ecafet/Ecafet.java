4ca3cbe784e7034237c3fe70fa6ded9382132313
andipermana444 <99400301+andipermana444@users.noreply.github.com>
2023-01-16 02:16:40 +0700

    awal dari sebuah projek

---
+++ Ecafet/src/main/java/com/mycompany/ecafet/Ecafet.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ecafet;
+
+/**
+ *
+ * @author LEGION
+ */
+public class Ecafet {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
